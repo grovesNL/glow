979ec7707315287ced58b3dac76459469ddd5111
Vlad <vladzhukovtirko@gmail.com>
2019-07-07 22:16:25 +0300

    Change i8 to c_char

--- src/native.rs
+++ src/native.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::os::raw::c_char;
 
 use std::ffi::CString;
 
@@ -389,7 +390,7 @@ impl super::Context for Context {
         name: &str,
     ) {
         let gl = &self.raw;
-        gl.BindFragDataLocation(program, color_number, name.as_ptr() as *const i8);
+        gl.BindFragDataLocation(program, color_number, name.as_ptr() as *const c_char);
     }
 
     unsafe fn buffer_data_size(&self, target: u32, size: i32, usage: u32) {
@@ -796,7 +797,7 @@ impl super::Context for Context {
     unsafe fn get_parameter_indexed_string(&self, parameter: u32, index: u32) -> String {
         let gl = &self.raw;
         let raw_ptr = gl.GetStringi(parameter, index);
-        std::ffi::CStr::from_ptr(raw_ptr as *const i8)
+        std::ffi::CStr::from_ptr(raw_ptr as *const c_char)
             .to_str()
             .unwrap()
             .to_owned()
@@ -805,7 +806,7 @@ impl super::Context for Context {
     unsafe fn get_parameter_string(&self, parameter: u32) -> String {
         let gl = &self.raw;
         let raw_ptr = gl.GetString(parameter);
-        std::ffi::CStr::from_ptr(raw_ptr as *const i8)
+        std::ffi::CStr::from_ptr(raw_ptr as *const c_char)
             .to_str()
             .unwrap()
             .to_owned()
@@ -818,7 +819,7 @@ impl super::Context for Context {
     ) -> Option<Self::UniformLocation> {
         let gl = &self.raw;
         let name = CString::new(name).unwrap();
-        let uniform_location = gl.GetUniformLocation(program, name.as_ptr() as *const i8);
+        let uniform_location = gl.GetUniformLocation(program, name.as_ptr() as *const c_char);
         if uniform_location < 0 {
             None
         } else {
@@ -829,13 +830,13 @@ impl super::Context for Context {
     unsafe fn get_attrib_location(&self, program: Self::Program, name: &str) -> i32 {
         let gl = &self.raw;
         let name = CString::new(name).unwrap();
-        gl.GetAttribLocation(program, name.as_ptr() as *const i8) as i32
+        gl.GetAttribLocation(program, name.as_ptr() as *const c_char) as i32
     }
 
     unsafe fn bind_attrib_location(&self, program: Self::Program, index: u32, name: &str) {
         let gl = &self.raw;
         let name = CString::new(name).unwrap();
-        gl.BindAttribLocation(program, index, name.as_ptr() as *const i8);
+        gl.BindAttribLocation(program, index, name.as_ptr() as *const c_char);
     }
 
     unsafe fn get_sync_status(&self, fence: Self::Fence) -> u32 {
@@ -1609,7 +1610,7 @@ impl super::Context for Context {
             id,
             severity,
             length,
-            message.as_ptr() as *const i8,
+            message.as_ptr() as *const c_char,
         );
     }
 
@@ -1678,7 +1679,7 @@ impl super::Context for Context {
         let gl = &self.raw;
         let msg = message.as_ref().as_bytes();
         let length = msg.len() as i32;
-        gl.PushDebugGroup(source, id, length, msg.as_ptr() as *const i8);
+        gl.PushDebugGroup(source, id, length, msg.as_ptr() as *const c_char);
     }
 
     unsafe fn pop_debug_group(&self) {
@@ -1696,7 +1697,7 @@ impl super::Context for Context {
             Some(l) => {
                 let lbl = l.as_ref().as_bytes();
                 let length = lbl.len() as i32;
-                gl.ObjectLabel(identifier, name, length, lbl.as_ptr() as *const i8);
+                gl.ObjectLabel(identifier, name, length, lbl.as_ptr() as *const c_char);
             }
             None => gl.ObjectLabel(identifier, name, 0, std::ptr::null()),
         }
@@ -1733,7 +1734,7 @@ impl super::Context for Context {
                 gl.ObjectPtrLabel(
                     sync as *mut std::ffi::c_void,
                     length,
-                    lbl.as_ptr() as *const i8
+                    lbl.as_ptr() as *const c_char
                 );
             }
             None => gl.ObjectPtrLabel(sync as *mut std::ffi::c_void, 0, std::ptr::null()),
@@ -1764,7 +1765,7 @@ extern "system" fn raw_debug_message_callback<F>(
     id: u32,
     severity: u32,
     length: i32,
-    message: *const i8,
+    message: *const c_char,
     user_param: *mut std::ffi::c_void,
 )
 where
